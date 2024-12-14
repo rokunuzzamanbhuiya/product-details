db8105b051ce3a5ea7fffc4df9b9d029114c290c
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2024-12-14 22:17:23 +0600

    sass, bootstrap, fontawesome, font added

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+  ("use strict");
+
+  /*
+  Template Name: Restaurant Website - Taste The Authentic Saudi Cuisine
+  Author: Rokunuzzaman Bhuiya
+  Description: Restaurant Website - Taste The Authentic Saudi Cuisine
+  File Description: Main JS file of the template
+*/
+})(jQuery);
